65db1780ae4a4023be927e61869a8a0e7788db6a
Titou-Dh <titoudh1@gmail.com>
2024-07-17 12:05:03 +0100

    MODIFIED CONNECTION

--- bijouterie/php/connexion.php
+++ bijouterie/php/connexion.php
@@ -4,8 +4,9 @@ if(!isset($_SESSION["user"])){
     header("Location: index.php");
     exit();
 }
-$cnx = mysqli_connect("localhost", "root", "", "bijouterie");
+// $cnx = mysqli_connect("localhost", "root", "", "bijouterie");
 
+$cnx = mysqli_connect("sql206.infinityfree.com", "if0_36914670", "gfNPleBx2O", "if0_36914670_bijouterie");
 
 
 
